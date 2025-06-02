953dbdd196aff3978909dae9c14728ea1a2405ae
rakaputar <ikhramraka@gmail.com>
2025-06-02 16:27:43 +0700

    bismillah

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@ FROM node:20-alpine
 WORKDIR /app
 COPY . .
 RUN npm install
+RUN npm install @google-cloud/storage
+# RUN node seed.js
 EXPOSE 5000
+CMD ["node", "seed.js"]
 CMD ["node", "index.js"]
--- config/db.js
+++ config/db.js
@@ -1,6 +1,20 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
+import { Storage } from '@google-cloud/storage';
+import dotenv from 'dotenv';
+import { Readable } from 'stream';
+
+const storage = new Storage();
+const bucketName = 'projek_tcc';
+const fileName = '/.env';
+
+async function loadEnvFromBucket() {
+  const file = storage.bucket(bucketName).file(fileName);
+  const contents = await file.download();
+  const envStream = Readable.from(contents[0].toString());
+  dotenv.parse(envStream);
+}
+
+await loadEnvFromBucket();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
